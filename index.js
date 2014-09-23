c5efbf02c8500b9f62c977ee7736a4730b765c16
David Wickes <dave.wickes@gmail.com>
2014-09-23 22:18:26 +0100

    opens first web socket. Loads a js script from a directory that doesn't exist -express trickery?

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 var app = require('express')();
 var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
 app.get('/', function(request, response){
 	var options = { root: './' };
 	response.sendFile('index.html', options);
+});
+
+io.on('connection', function(socket){
+	console.log('a user connected');
+	socket.on('disconnect', function(){
+		console.log('a user disconnected')
+	});
+	socket.on('chat message', function(msg){
+		console.log('message :' + msg);
+	});
 });
 
 http.listen(3000, function(){
